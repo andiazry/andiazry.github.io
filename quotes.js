bea3e5dd7fcbd7f293e37d2a1cb29ae75c99d106
Andi Azry <andiazry@gmail.com>
2019-05-15 22:05:54 -0400

    Added json

--- quotes.js
+++ quotes.js
@@ -1,8 +1,16 @@
 const quoteTag = document.querySelector("h1");
+const authorTag = document.querySelector("p");
 const randomTag = document.querySelector("footer img");
 
+let data = [];
+
 const getQuote = function() {
-  quoteTag.innerHTML = "loaded with javascript " + Math.random();
+  if (data.length > 0) {
+    const randomNumber = Math.floor(Math.random() * data.length);
+    const randomQuote = data[randomNumber];
+    quoteTag.innerHTML = randomQuote.quote;
+    authorTag.innerHTML = randomQuote.author;
+  }
 };
 
 getQuote();
