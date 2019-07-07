9c715d154018fa31033450d7920bec518654ac21
Andi Azry <andiazry@gmail.com>
2019-07-07 16:49:13 -0400

    added other currency

--- coin.js
+++ coin.js
@@ -7,8 +7,21 @@ const checkPrice = function() {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      priceTag.innerHTML = data.bpi.USD.rate_float.toFixed(1);
+      priceTag.innerHTML = data.bpi[currency].rate_float.toFixed(1);
     });
 };
 
 checkPrice();
+
+// loop over nav links and add click event
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", function() {
+    currency = this.getAttribute("data-currency");
+    checkPrice();
+
+    navLinks.forEach(link => link.classList.remove("selected"));
+    this.classList.add("selected");
+  });
+});
